aa3ed28b8f9e409983c62ae09d5efeeedecd2763
hannahsaintonge <hannahsaintonge@gmail.com>
2018-03-08 14:20:17 -0500

    First commit for Deliverable2Lab2

---
+++ script.js
@@ -0,0 +1,76 @@
+//////////////////////////////////////////////////////
+/* EXERCISE ONE */
+
+var userNumber = prompt("Pick a number!");
+var addedNumber = 0;
+
+for (var i = 0; i <= userNumber; i++) {
+    addedNumber = (addedNumber + i);
+}
+console.log(userNumber + " is " + addedNumber);
+
+
+
+//////////////////////////////////////////////////////
+/* EXERCISE TWO */
+
+var sentence = (" ");
+var words = ("");
+do {
+    var choice = prompt("Do you want to play?");
+    if (choice === "no") {
+        break;
+    }
+    while (choice != "yes" && choice != "no") {
+        choice = prompt("Please enter yes or no");
+
+    }
+    if (choice === "yes") {
+        var words = prompt("Enter a word.");
+        sentence = (sentence + " " + words);
+    }}
+while (choice != "no");
+console.log(sentence);
+
+
+
+//////////////////////////////////////////////////////
+/* EXERCISE THREE */
+
+var choice = prompt("Would you like to print your name?");
+if (choice != "yes") {
+
+}
+if (choice === "yes") {
+    var name = prompt("Enter your name.");
+    console.log("Hello, my name is " + name);
+    var again = prompt("Would you like to print your name again?")
+    while (again === "yes") {
+        var name = (name + "!");
+        console.log("hello, my name is " + name);
+        var again = prompt("Would you like to print your name again?")
+    }
+    if (again != "yes") {
+        console.log(":)");
+    }
+}
+
+
+
+//////////////////////////////////////////////////////
+/* EXERCISE FOUR */
+
+while (time != "morning" && time != "noon" && time != "evening") {
+    var time = prompt("What time of day is it?");
+}
+if (time === "morning") {
+    console.log("Since it's morning, you should be eating breakfast. We suggest eggs and toast.");
+}
+else if (time === "noon") {
+    console.log("Since it's noon, you should be eating lunch. We suggest a salad.");
+}
+else if (time === "evening") {
+    console.log("Since it's evening, you should be eating dinner. We suggest chicken and rice.");
+}
+
+
